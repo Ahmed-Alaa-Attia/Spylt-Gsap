707d967590b1bfdb50833608713e3e39c52cf9ee
Ahmed Alaa <ahmed.alaa1362001@gmail.com>
2025-07-19 03:09:07 +0300

    Flavor section animation

--- src/components/FlavorTitle.jsx
+++ src/components/FlavorTitle.jsx
@@ -1,17 +1,63 @@
+import { useGSAP } from "@gsap/react";
+import gsap, { SplitText } from "gsap/all";
+
 const FlavorTitle = () => {
+  useGSAP(() => {
+    const firstTextSplit = SplitText.create(".first-text-split", {
+      type: "chars",
+    });
+
+    const secondTextSplit = SplitText.create(".second-text-split", {
+      type: "chars",
+    });
+
+    gsap.from(firstTextSplit.chars, {
+      yPercent: 200,
+      opacity: 0,
+      ease: "power1.inOut",
+      stagger: 0.02,
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "50% bottom",
+      },
+    });
+
+    gsap.to(".flavor-text-scroll", {
+      duration: 1,
+      clipPath: "polygon(100% 0%, 0% 0%, 0% 100%, 100% 100%)",
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "67% bottom",
+      },
+    });
+
+    gsap.from(secondTextSplit.chars, {
+      yPercent: 200,
+      opacity: 0,
+      ease: "power1.inOut",
+      stagger: 0.02,
+      scrollTrigger: {
+        trigger: ".flavor-section",
+        start: "85% bottom",
+      },
+    });
+  });
   return (
     <div className="general-title col-center h-full 2xl:gap-32 xl:gap-24 gap-16">
       <div className="overflow-hidden 2xl:py-0 py-3 first-text-split">
         <h1>We have 6</h1>
       </div>
 
-      <div className="flavor-text-scroll">
+      <div
+        style={{ clipPath: "polygon(50% 0%, 50% 0%, 50% 100%, 50% 100%)" }}
+        className="flavor-text-scroll"
+      >
         <div className="bg-mid-brown pb-5 2xl:pt-0 pt-3 2xl:px-5 px-3">
           <h2 className="text-milk">freaking</h2>
         </div>
       </div>
 
-      <div className="overflow-hidden 2xl:py-0 py-3 secound-text-split">
+      <div className="overflow-hidden 2xl:py-0 py-3 second-text-split">
         <h1>delicious flavors</h1>
       </div>
     </div>
